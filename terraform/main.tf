11a1ccb84856b314231d13c996ad1e33d6ba84fb
Garv <garv1908@gmail.com>
2025-01-19 03:08:32 -0500

    figure out docker and terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,13 @@ provider "docker" {
   host = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "application_image" {
-  name = "application"
-  build {
-    context    = "${path.module}/../"
-    dockerfile = "${path.module}/../Dockerfile"
-  }
+resource "docker_image" "talktrails" {
+  name = "garv1908/talktrails:v1.0"
 }
 
 resource "docker_container" "application_container" {
   name  = "frontend"
-  image = docker_image.application_image.image_id
+  image = docker_image.talktrails.image_id
   ports {
     internal = 5173
     external = 3000
